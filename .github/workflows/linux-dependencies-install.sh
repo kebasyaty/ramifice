e4f7f3c21ef1e215870f4794c82a3a001933e857
kebasyaty <kebasyaty@gmail.com>
2024-12-08 07:48:59 +0200

    update linux-dependencies-install.sh

--- .github/workflows/linux-dependencies-install.sh
+++ .github/workflows/linux-dependencies-install.sh
@@ -1,21 +1,19 @@
-# #!/bin/bash
+#!/bin/bash
 
-# set -e
+set -e
 
-# # Install sudo on Linux
-# apt update
-# apt install -y sudo
+apt update
 
-# # Install OS dependencies
-# sudo apt install -y curl gnupg2 systemctl git
+# Install OS dependencies
+apt install -y curl gnupg2 systemctl git
 
-# # Install MongoDB server
-# curl -fsSL https://www.mongodb.org/static/pgp/server-8.0.asc | \
-#     sudo gpg -o /usr/share/keyrings/mongodb-server-8.0.gpg --dearmor
-# echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-8.0.gpg ] \
-#     https://repo.mongodb.org/apt/ubuntu noble/mongodb-org/8.0 multiverse" | \
-#     sudo tee /etc/apt/sources.list.d/mongodb-org-8.0.list
-# sudo apt update
-# sudo apt install -y mongodb-org
-# mongod --version
-# sudo systemctl start mongod
+# Install MongoDB server
+curl -fsSL https://www.mongodb.org/static/pgp/server-8.0.asc | \
+    gpg -o /usr/share/keyrings/mongodb-server-8.0.gpg --dearmor
+echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-8.0.gpg ] \
+    https://repo.mongodb.org/apt/ubuntu noble/mongodb-org/8.0 multiverse" | \
+    tee /etc/apt/sources.list.d/mongodb-org-8.0.list
+apt update
+apt install -y mongodb-org
+mongod --version
+systemctl start mongod
