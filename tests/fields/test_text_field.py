4acf981305b1b7cd415feef11e56420d0a5fcbb6
kebasyaty <kebasyaty@gmail.com>
2024-12-08 18:18:37 +0200

    update test_text_field.py

---
+++ tests/fields/test_text_field.py
@@ -0,0 +1,25 @@
+"""Testing TextField."""
+
+import unittest
+from ramifice.fields import text_field
+
+
+class TestTextField(unittest.TestCase):
+    """Testing TextField."""
+
+    def test_attributes_by_default(self):
+        """Testing atrabuts with default values."""
+        f = text_field.TextField()
+        self.assertEqual(f.id, "")
+        self.assertEqual(f.label, "")
+        self.assertEqual(f.field_type, "TextField")
+        self.assertFalse(f.disabled)
+        self.assertFalse(f.hide)
+        self.assertFalse(f.ignored)
+        self.assertIsNone(f.warning)
+        self.assertIsNone(f.errors)
+        self.assertEqual(f.group, 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
