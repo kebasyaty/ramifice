f080b202842b43154561250eb539b9cf2c3aa4eb
kebasyaty <kebasyaty@gmail.com>
2024-12-10 13:46:49 +0200

    Add SlugField

--- ramifice/fields/fields.py
+++ ramifice/fields/fields.py
@@ -2,3 +2,4 @@
 
 from .text_field import TextField  # pylint: disable=unused-import
 from .url_field import URLField  # pylint: disable=unused-import
+from .slug_field import SlugField  # pylint: disable=unused-import
