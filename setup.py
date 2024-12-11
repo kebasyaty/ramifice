b309e774b72b849855e82fc54f21340d4223507d
kebasyaty <kebasyaty@gmail.com>
2024-12-11 19:29:48 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 msg = (
-    "Ramifice no supports building via setup.py, use python -m pip install <path/to/pymongo> instead. If "
+    "Ramifice no supports building via setup.py, use python -m pip install <path/to/ramifice> instead. If "
     "this is an editable install (-e) please upgrade to pip>=21.3 first: python -m pip install --upgrade pip"
 )
 
