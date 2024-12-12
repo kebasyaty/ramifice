214dc9c08f84aa12ca5b86579651012be8e3eadc
kebasyaty <kebasyaty@gmail.com>
2024-12-12 17:07:25 +0200

    update PhoneField

--- ramifice/_fields/phone_field.py
+++ ramifice/_fields/phone_field.py
@@ -1,9 +1,10 @@
 """Field of Model for enter phone number."""
 
-from .general import (field, text_group)
+from .general.field import Field
+from .general.text_group import TextGroup
 
 
-class PhoneField(field.Field, text_group.TextGroup):
+class PhoneField(Field, TextGroup):
     """Field of Model for enter phone number.
     WARNING: By default is used validator `Valid.phone_number?`.
     Examples:
@@ -28,28 +29,27 @@ class PhoneField(field.Field, text_group.TextGroup):
                  regex: str = '',
                  regex_err_msg: list[str] | None = None,
                  ):
-        field.Field.__init__(self,
-                             label=label,
-                             disabled=disabled,
-                             hide=hide,
-                             ignored=ignored,
-                             hint=hint,
-                             warning=warning,
-                             field_type='PhoneField',
-                             group='text',
-                             )
-        text_group.TextGroup.__init__(self,
-                                      input_type='tel',
-                                      default=default,
-                                      placeholder=placeholder,
-                                      required=required,
-                                      readonly=readonly,
-                                      unique=unique,
-                                      )
+        Field.__init__(self,
+                       label=label,
+                       disabled=disabled,
+                       hide=hide,
+                       ignored=ignored,
+                       hint=hint,
+                       warning=warning,
+                       field_type='PhoneField',
+                       group='text',
+                       )
+        TextGroup.__init__(self,
+                           input_type='tel',
+                           default=default,
+                           placeholder=placeholder,
+                           required=required,
+                           readonly=readonly,
+                           unique=unique,
+                           )
         self.__regex = regex
         self.__regex_err_msg = regex_err_msg
 
-    # --------------------------------------------------------------------------
     @property
     def regex(self) -> str:
         """Regular expression to validate the `value`.
@@ -57,7 +57,6 @@ class PhoneField(field.Field, text_group.TextGroup):
         """
         return self.__regex
 
-    # --------------------------------------------------------------------------
     @property
     def regex_err_msg(self) -> list[str] | None:
         """Error message.
