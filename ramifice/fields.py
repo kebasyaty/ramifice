95f73b10fd1792830decab9b682b096614613198
kebasyaty <kebasyaty@gmail.com>
2024-12-12 17:24:14 +0200

    add HashField

--- ramifice/fields.py
+++ ramifice/fields.py
@@ -6,3 +6,4 @@ from ._fields.slug_field import SlugField  # pylint: disable=unused-import
 from ._fields.phone_field import PhoneField  # pylint: disable=unused-import
 from ._fields.password_field import PasswordField  # pylint: disable=unused-import
 from ._fields.ip_field import IPField  # pylint: disable=unused-import
+from ._fields.hash_field import HashField  # pylint: disable=unused-import
