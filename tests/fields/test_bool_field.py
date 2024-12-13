f0344f486c085f3bb17301cb82ad2023fccb7c63
kebasyaty <kebasyaty@gmail.com>
2024-12-13 14:43:31 +0200

    add test for BoolField - parameters with default values

---
+++ tests/fields/test_bool_field.py
@@ -0,0 +1,28 @@
+"""Testing a parameters with default values for logical (boolean) field."""
+
+import unittest
+from ramifice.fields import BoolField
+
+
+class TestTextFields(unittest.TestCase):
+    """Testing parameters with default values."""
+
+    def test_bool_field(self):
+        """Testing a parameters by default for BoolField."""
+        f = BoolField()
+        self.assertEqual(f.id, "")
+        self.assertEqual(f.label, "")
+        self.assertEqual(f.name, "")
+        self.assertEqual(f.field_type, "BoolField")
+        self.assertFalse(f.disabled)
+        self.assertFalse(f.hide)
+        self.assertFalse(f.ignored)
+        self.assertIsNone(f.warning)
+        self.assertIsNone(f.errors)
+        self.assertEqual(f.group, 'bool')
+        self.assertEqual(f.input_type, 'checkbox')
+        self.assertIsNone(f.value)
+
+
+if __name__ == '__main__':
+    unittest.main()
