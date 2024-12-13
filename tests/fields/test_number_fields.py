3808ae1a44791ed0fc06f4d529d37342a4668296
kebasyaty <kebasyaty@gmail.com>
2024-12-13 18:02:06 +0200

    add test for FloatField - parameters by default

--- tests/fields/test_number_fields.py
+++ tests/fields/test_number_fields.py
@@ -1,7 +1,7 @@
 """Testing a parameters with default values for number fields."""
 
 import unittest
-from ramifice.fields import (IntegerField)
+from ramifice.fields import (IntegerField, FloatField)
 
 
 class TestTextFields(unittest.TestCase):
@@ -30,6 +30,30 @@ class TestTextFields(unittest.TestCase):
         self.assertIsNone(f.max_number)
         self.assertIsNone(f.min_number)
         self.assertEqual(f.step, int(1))
+
+    def test_float_field(self):
+        """Testing a parameters by default for FloatField."""
+        f = FloatField()
+        self.assertEqual(f.id, "")
+        self.assertEqual(f.label, "")
+        self.assertEqual(f.name, "")
+        self.assertEqual(f.field_type, 'FloatField')
+        self.assertFalse(f.disabled)
+        self.assertFalse(f.hide)
+        self.assertFalse(f.ignored)
+        self.assertIsNone(f.warning)
+        self.assertIsNone(f.errors)
+        self.assertEqual(f.group, 'float')
+        self.assertEqual(f.input_type, 'number')
+        self.assertIsNone(f.value)
+        self.assertIsNone(f.default)
+        self.assertEqual(f.placeholder, "")
+        self.assertFalse(f.required)
+        self.assertFalse(f.readonly)
+        self.assertFalse(f.unique)
+        self.assertIsNone(f.max_number)
+        self.assertIsNone(f.min_number)
+        self.assertEqual(f.step, float(1))
 
 
 if __name__ == '__main__':
