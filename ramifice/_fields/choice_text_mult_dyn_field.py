8f298a6d2ec5d599eed770f7032800a0d1f556a6
kebasyaty <kebasyaty@gmail.com>
2024-12-14 19:07:30 +0200

    add ChoiceTextMultDynField

---
+++ ramifice/_fields/choice_text_mult_dyn_field.py
@@ -0,0 +1,74 @@
+"""Field of Model.
+Type of selective field with dynamic addition of elements.
+"""
+
+from .general.field import Field
+from .general.choice_group import ChoiceGroup
+
+
+class ChoiceTextMultDynField(Field, ChoiceGroup):
+    """Field of Model.
+    Type of selective field with dynamic addition of elements.
+    For simulate relationship Many-to-Many.
+    How to use, see <a href="https://github.com/kebasyaty/ramifice/tree/main/examples/dynamic_choices" target="_blank">example</a>.
+    """
+
+    def __init__(self,
+                 label: str = "",
+                 disabled: bool = False,
+                 hide: bool = False,
+                 ignored: bool = False,
+                 hint: str = "",
+                 warning: list[str] | None = None,
+                 required: bool = False,
+                 readonly: bool = False,
+                 choices: list[tuple[str]] | None = None
+                 ):
+        Field.__init__(self,
+                       label=label,
+                       disabled=disabled,
+                       hide=hide,
+                       ignored=ignored,
+                       hint=hint,
+                       warning=warning,
+                       field_type='ChoiceTextMultDynField',
+                       group='choice',
+                       )
+        ChoiceGroup.__init__(self,
+                             required=required,
+                             readonly=readonly,
+                             multiple=True,
+                             )
+        self.__value: list[str] | None = None
+        self.__choices = choices
+
+    @property
+    def value(self) -> list[str] | None:
+        """Sets the value of an element."""
+        return self.__value
+
+    @value.setter
+    def value(self, value: list[str] | None) -> None:
+        self.__value = value
+
+    # --------------------------------------------------------------------------
+    @property
+    def choices(self) -> list[tuple[str]] | None:
+        """ Html tag: select.
+        Example: [('value', 'Title'), ('value 2', 'Title 2')]
+        """
+        return self.__choices
+
+    # --------------------------------------------------------------------------
+    def has_value(self) -> bool:
+        """Does the field value match the possible options in choices."""
+        flag = True
+        value = self.__value
+        choices = self.__choices
+        if value is not None and choices is not None:
+            value_list = [item[0] for item in choices]
+            for item in value:
+                if item not in value_list:
+                    flag = False
+                    break
+        return flag
