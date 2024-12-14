92ccfe24508ba0bb37c9b9ea963b7378fb8d8dd4
kebasyaty <kebasyaty@gmail.com>
2024-12-14 08:43:38 +0200

    add test for DateTimeField - parameters by default

--- tests/fields/test_date_fields.py
+++ tests/fields/test_date_fields.py
@@ -1,7 +1,7 @@
 """Testing a parameters with default values for date|datetime fields."""
 
 import unittest
-from ramifice.fields import (DateField)
+from ramifice.fields import (DateField, DateTimeField)
 
 
 class TestTextFields(unittest.TestCase):
@@ -21,6 +21,29 @@ class TestTextFields(unittest.TestCase):
         self.assertIsNone(f.errors)
         self.assertEqual(f.group, 'date')
         self.assertEqual(f.input_type, 'date')
+        self.assertIsNone(f.value)
+        self.assertEqual(f.default, "")
+        self.assertEqual(f.placeholder, "")
+        self.assertFalse(f.required)
+        self.assertFalse(f.readonly)
+        self.assertFalse(f.unique)
+        self.assertIsNone(f.max_date)
+        self.assertIsNone(f.min_date)
+
+    def test_date_time_field(self):
+        """Testing a parameters by default for DateTimeField."""
+        f = DateTimeField()
+        self.assertEqual(f.id, "")
+        self.assertEqual(f.label, "")
+        self.assertEqual(f.name, "")
+        self.assertEqual(f.field_type, 'DateTimeField')
+        self.assertFalse(f.disabled)
+        self.assertFalse(f.hide)
+        self.assertFalse(f.ignored)
+        self.assertIsNone(f.warning)
+        self.assertIsNone(f.errors)
+        self.assertEqual(f.group, 'date')
+        self.assertEqual(f.input_type, 'datetime')
         self.assertIsNone(f.value)
         self.assertEqual(f.default, "")
         self.assertEqual(f.placeholder, "")
