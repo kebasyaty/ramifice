4713efbe8539e03f74d5fe50e6be50484fb427bd
kebasyaty <kebasyaty@gmail.com>
2024-12-14 14:23:59 +0200

    add test for ChoiceTextMultField - parameters by default

--- tests/fields/test_choice_fields.py
+++ tests/fields/test_choice_fields.py
@@ -1,7 +1,7 @@
 """Testing a parameters with default values for selective field."""
 
 import unittest
-from ramifice.fields import (ChoiceTextField)
+from ramifice.fields import (ChoiceTextField, ChoiceTextMultField)
 
 
 class TestTextFields(unittest.TestCase):
@@ -27,6 +27,31 @@ class TestTextFields(unittest.TestCase):
         self.assertFalse(f.readonly)
         self.assertFalse(f.unique)
         self.assertFalse(f.multiple)
+        #
+        self.assertIsNone(f.has_value)
+
+    def test_choice_text_mult_field(self):
+        """Testing a parameters by default for ChoiceTextMultField."""
+        f = ChoiceTextMultField()
+        self.assertEqual(f.id, "")
+        self.assertEqual(f.label, "")
+        self.assertEqual(f.name, "")
+        self.assertEqual(f.field_type, 'ChoiceTextMultField')
+        self.assertFalse(f.disabled)
+        self.assertFalse(f.hide)
+        self.assertFalse(f.ignored)
+        self.assertIsNone(f.warning)
+        self.assertIsNone(f.errors)
+        self.assertEqual(f.group, 'choice')
+        self.assertIsNone(f.value)
+        self.assertIsNone(f.default)
+        self.assertEqual(f.placeholder, "")
+        self.assertFalse(f.required)
+        self.assertFalse(f.readonly)
+        self.assertFalse(f.unique)
+        self.assertTrue(f.multiple)
+        #
+        self.assertIsNone(f.has_value)
 
 
 if __name__ == '__main__':
