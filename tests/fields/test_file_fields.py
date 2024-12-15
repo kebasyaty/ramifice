40b0c4dcaccd6e396f8066aba3d966411e16fe3a
kebasyaty <kebasyaty@gmail.com>
2024-12-15 09:39:18 +0200

    add test_file_field

---
+++ tests/fields/test_file_fields.py
@@ -0,0 +1,32 @@
+"""Testing a parameters with default values for file fields."""
+
+import unittest
+from ramifice.fields import (FileField)
+
+
+class TestFileFields(unittest.TestCase):
+    """Testing parameters with default values."""
+
+    def test_file_field(self):
+        """Testing a parameters by default for FileField."""
+        f = FileField()
+        self.assertEqual(f.id, "")
+        self.assertEqual(f.label, "")
+        self.assertEqual(f.name, "")
+        self.assertEqual(f.field_type, 'FileField')
+        self.assertFalse(f.disabled)
+        self.assertFalse(f.hide)
+        self.assertFalse(f.ignored)
+        self.assertIsNone(f.warning)
+        self.assertIsNone(f.errors)
+        self.assertEqual(f.group, 'file')
+        self.assertEqual(f.input_type, 'file')
+        self.assertIsNone(f.value)
+        self.assertIsNone(f.default)
+        self.assertFalse(f.required)
+        self.assertEqual(f.target_dir, "")
+        self.assertEqual(f.accept, "")
+
+
+if __name__ == '__main__':
+    unittest.main()
