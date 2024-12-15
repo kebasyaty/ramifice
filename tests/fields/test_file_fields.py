1b61fa6d0b255edb4642ca1c2feef70d64de51a4
kebasyaty <kebasyaty@gmail.com>
2024-12-15 09:56:07 +0200

    add test for ImageField - parameters by default

--- tests/fields/test_file_fields.py
+++ tests/fields/test_file_fields.py
@@ -1,7 +1,7 @@
 """Testing a parameters with default values for file fields."""
 
 import unittest
-from ramifice.fields import (FileField)
+from ramifice.fields import (FileField, ImageField)
 
 
 class TestFileFields(unittest.TestCase):
@@ -20,6 +20,29 @@ class TestFileFields(unittest.TestCase):
         self.assertIsNone(f.warning)
         self.assertIsNone(f.errors)
         self.assertEqual(f.group, 'file')
+        self.assertEqual(f.input_type, 'file')
+        self.assertIsNone(f.value)
+        self.assertIsNone(f.default)
+        self.assertEqual(f.placeholder, "")
+        self.assertFalse(f.required)
+        self.assertEqual(f.target_dir, "")
+        self.assertEqual(f.accept, "")
+        self.assertEqual(f.media_root, 'public/media/uploads')
+        self.assertEqual(f.media_url, '/media/uploads')
+
+    def test_image_field(self):
+        """Testing a parameters by default for ImageField."""
+        f = ImageField()
+        self.assertEqual(f.id, "")
+        self.assertEqual(f.label, "")
+        self.assertEqual(f.name, "")
+        self.assertEqual(f.field_type, 'ImageField')
+        self.assertFalse(f.disabled)
+        self.assertFalse(f.hide)
+        self.assertFalse(f.ignored)
+        self.assertIsNone(f.warning)
+        self.assertIsNone(f.errors)
+        self.assertEqual(f.group, 'image')
         self.assertEqual(f.input_type, 'file')
         self.assertIsNone(f.value)
         self.assertIsNone(f.default)
