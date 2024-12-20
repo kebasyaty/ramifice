6b4dafb23a145d1ac4d87be7357ca481068b65aa
kebasyaty <kebasyaty@gmail.com>
2024-12-20 09:50:28 +0200

    add ChoiceTextField.debug

--- ramifice/_fields/choice_text_field.py
+++ ramifice/_fields/choice_text_field.py
@@ -47,6 +47,18 @@ class ChoiceTextField(Field, ChoiceGroup):
         self.__default = default
         self.__choices = choices
 
+        if ChoiceTextField.debug:
+            if choices is not None and not isinstance(choices, list):
+                raise AssertionError(
+                    'Parameter `choices` - Not а `list` type!')
+            if default is not None and not isinstance(default, str):
+                raise AssertionError(
+                    'Parameter `default` - Not а `str` type!')
+            if default is not None and choices is not None and not self.has_value():
+                raise AssertionError(
+                    'Parameter `default` does not coincide with ' +
+                    'list of permissive values in `choicees`.')
+
     @property
     def value(self) -> str | None:
         """Sets value of field."""
