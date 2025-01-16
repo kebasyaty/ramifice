a501f2ce92c7135f1b5c22940b3957cefde8ec70
kebasyaty <kebasyaty@gmail.com>
2025-01-16 10:26:22 +0200

    fix docstring in `def test_class_model`

--- tests/globals/test_model.py
+++ tests/globals/test_model.py
@@ -9,7 +9,7 @@ class TestModel(unittest.TestCase):
     """Testing the module `ramifice.model`."""
 
     def test_class_model(self):
-        """Testing a class `OutputData`."""
+        """Testing a class `Model`."""
         self.assertEqual(Model.__name__, "Model")
         self.assertEqual(Model.__module__, "ramifice.model")
         self.assertIsNotNone(Model.__dict__.get("hash"))
