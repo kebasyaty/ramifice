234f623a18ff9ca354d4929f0cde04a0f7957a1f
kebasyaty <kebasyaty@gmail.com>
2025-01-20 13:53:42 +0200

    update migration

--- ramifice/migration.py
+++ ramifice/migration.py
@@ -3,3 +3,20 @@ propagating changes you make to
 your models (add or delete a Model, add or delete a field in Model, etc.) into
 your database schema.
 """
+
+
+class ModelState:
+    """For control state of Model in the super collection."""
+
+    def __init__(self):
+        self.collection_name = ""
+        self.field_name_and_type_list = {}
+        self.data_dynamic_fields = {}
+        self.model_exists = False
+
+
+class Monitor:
+    """Monitoring and updating database state for application."""
+
+    def __init__(self, database_name: str):
+        pass
