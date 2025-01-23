b5e3bc3dae67093f04210f7aaa1eee6eb1bd0d50
kebasyaty <kebasyaty@gmail.com>
2025-01-23 10:53:06 +0200

    update TestAsyncMongoClient

--- tests/test_mongo.py
+++ tests/test_mongo.py
@@ -1,13 +1,24 @@
-"""Testing the module `ramifice.store`."""
+"""Testing the `AsyncMongoClient`."""
 
 import unittest
 
 from pymongo import AsyncMongoClient
 
 
-class TestAsyncMongoClient(unittest.TestCase):
+class TestAsyncMongoClient(unittest.IsolatedAsyncioTestCase):
     """Testing the `AsyncMongoClient`."""
 
-    def test_connection_mongo_client(self):
+    async def test_connection_mongo_client(self):
         """Connection with AsyncMongoClient."""
-        client = AsyncMongoClient("localhost", 27017)
+        # Making a Connection with MongoClient:
+        client = AsyncMongoClient(host="localhost", port=27017)
+        # Getting a Database:
+        db = client["test-database"]
+        self.assertEqual(db.name, "test-database")
+        # Getting a Collection:
+        collection = db["test-collection"]
+        # Inserting a Document:
+        await collection.insert_one({"x": 1})
+        self.assertEqual(await db.list_collection_names(), ["test-collection"])
+        #
+        await client.close()
