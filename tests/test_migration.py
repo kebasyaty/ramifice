3854147a627f0c8ce51a5cd911276c02c0932493
kebasyaty <kebasyaty@gmail.com>
2025-01-24 11:44:35 +0200

    reconfigur tests

---
+++ tests/test_migration.py
@@ -0,0 +1,19 @@
+"""Testing the module `ramifice.migration`."""
+
+import unittest
+
+from ramifice.migration import ModelState
+
+# from pymongo import AsyncMongoClient
+
+
+class TestMigration(unittest.IsolatedAsyncioTestCase):
+    """Testing the module `ramifice.migration`."""
+
+    async def test_class_model_state(self):
+        """Testing a class `ModelState`."""
+        ms = ModelState()
+        self.assertEqual(ms.collection_name, "")
+        self.assertEqual(ms.field_name_and_type_list, {})
+        self.assertEqual(ms.data_dynamic_fields, {})
+        self.assertFalse(ms.model_exists)
