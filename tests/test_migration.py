6b6d8e5f877d172c9518cc3f493aacba513d665a
kebasyaty <kebasyaty@gmail.com>
2025-01-24 18:20:44 +0200

    fix test_class_model_state

--- tests/test_migration.py
+++ tests/test_migration.py
@@ -16,4 +16,4 @@ class TestMigration(unittest.IsolatedAsyncioTestCase):
         self.assertEqual(ms.collection_name, "")
         self.assertEqual(ms.field_name_and_type_list, {})
         self.assertEqual(ms.data_dynamic_fields, {})
-        self.assertFalse(ms.model_exists)
+        self.assertFalse(ms.is_model_exist)
