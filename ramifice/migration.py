239e61b082f30d9682ac6616e9944a15ccae0716
kebasyaty <kebasyaty@gmail.com>
2025-01-24 09:42:16 +0200

    update class Monitor

--- ramifice/migration.py
+++ ramifice/migration.py
@@ -4,6 +4,10 @@ your models (add or delete a Model, add or delete a field in Model, etc.) into
 your database schema.
 """
 
+from pymongo import AsyncMongoClient
+
+from . import errors, store
+
 
 class ModelState:
     """For control state of Model in the super collection."""
@@ -18,5 +22,11 @@ class ModelState:
 class Monitor:
     """Monitoring and updating database state for application."""
 
-    def __init__(self, database_name: str):
-        pass
+    def __init__(self, database_name: str, mongo_client: AsyncMongoClient):
+        db_name_regex = store.REGEX.get("database_name")
+        if db_name_regex is not None and db_name_regex.match(database_name) is None:
+            raise errors.DoesNotMatchRegexError("^[a-zA-Z][-_a-zA-Z0-9]{0,59}$")
+        #
+        store.DATABASE_NAME = database_name
+        store.MONGO_CLIENT = mongo_client
+        store.MONGO_DATABASE = store.MONGO_CLIENT[store.DATABASE_NAME]
