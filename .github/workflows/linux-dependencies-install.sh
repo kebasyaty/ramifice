6a59324718cbb45700392583865012a39033d831
kebasyaty <kebasyaty@gmail.com>
2025-01-25 13:21:23 +0200

    update test.yml

--- .github/workflows/linux-dependencies-install.sh
+++ .github/workflows/linux-dependencies-install.sh
@@ -9,5 +9,4 @@ apt install -y curl gnupg2 systemctl git
 
 # Run MongoDB server
 mongod --version
-systemctl status mongod --no-pager -l
-systemctl start mongod
+docker run --name mongodb -p 27017:27017 -d mongodb/mongodb-community-server:latest
