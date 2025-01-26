421eb535989ffd02ae8734535b05df594c2e02ee
kebasyaty <kebasyaty@gmail.com>
2025-01-26 16:05:22 +0200

    update TestJsonMixin

--- tests/test_mixins.py
+++ tests/test_mixins.py
@@ -4,11 +4,28 @@ import unittest
 
 from ramifice import Model, meta
 from ramifice.fields import ChoiceIntField
+from ramifice.mixins import JsonMixin
+
+
+class StandardTypes(JsonMixin):
+    """For testing standard types."""
+
+    def __init__(self):
+        JsonMixin.__init__(self)
+        self.x1 = {"x": 1, "y": 2}
+        self.x2 = [1, 2, 3]
+        self.x3 = (1, 2, 3)
+        self.x4 = "Hello world!"
+        self.x5 = 12
+        self.x6 = 12.3
+        self.x7 = True
+        self.x8 = False
+        self.x9 = None
 
 
 @meta(service_name="Profiles")
-class UserProfile(Model):
-    """Class for testing."""
+class User(Model):
+    """For testing the Ramifice fields."""
 
     def __init__(self):
         self.profession = ChoiceIntField(choices=[(1, "Musician"), (2, "Artist")])
@@ -22,13 +39,26 @@ class UserProfile(Model):
 class TestJsonMixin(unittest.TestCase):
     """Testing the class `JsonMixin`."""
 
-    def test_to_json(self):
-        """Testing a method `to_json`."""
-        x = 3
-        print(x.__class__)
-        m = UserProfile()
+    def test_standard_types(self):
+        """Testing standard types."""
+        x = StandardTypes()
+        json_str = x.to_json()
+        y = StandardTypes.from_json(json_str)
+        self.assertEqual(x.x1, y.x1)
+        self.assertEqual(x.x2, y.x2)
+        self.assertEqual(x.x3, (1, 2, 3))  # y.x3 == [1, 2, 3]
+        self.assertEqual(x.x4, y.x4)
+        self.assertEqual(x.x5, y.x5)
+        self.assertEqual(x.x6, y.x6)
+        self.assertEqual(x.x7, y.x7)
+        self.assertEqual(x.x8, y.x8)
+        self.assertEqual(x.x9, y.x9)
+
+    def test_ramifice_fields(self):
+        """Testing the Ramifice fields."""
+        m = User()
         json_str = m.to_json()
-        m2 = UserProfile.from_json(json_str)
+        m2 = User.from_json(json_str)
         self.assertEqual(m.profession.choices, [(1, "Musician"), (2, "Artist")])
         self.assertEqual(m2.profession.choices, [[1, "Musician"], [2, "Artist"]])
 
