7f58def758481a061265cd967e288b4cb616f7d2
kebasyaty <kebasyaty@gmail.com>
2025-01-30 15:16:34 +0200

    update text_group method

--- ramifice/paladins/groups/text_group.py
+++ ramifice/paladins/groups/text_group.py
@@ -21,7 +21,22 @@ class TextGroupMixin:
         value = field.value or field.default
         if value is None:
             if field.required:
-                self.accumulate_error("Required field!", params)  # type: ignore[attr-defined]
+                err_msg = "Required field !"
+                self.accumulate_error(err_msg, params)  # type: ignore[attr-defined]
             if params["is_save"]:
                 params["result_map"] = None
             return
+        # Validation the `maxlength` field attribute.
+        maxlength = field.__dict__.get("maxlength")
+        if maxlength is not None:
+            if len(value) > maxlength:
+                err_msg = f"The number {len(value)} must not be greater than max={maxlength} !"
+                self.accumulate_error(err_msg, params)  # type: ignore[attr-defined]
+        # Validation the `minlength` field attribute.
+        minlength = field.__dict__.get("minlength")
+        if minlength is not None:
+            if len(value) < minlength:
+                err_msg = (
+                    f"The number {len(value)} must not be less than min={minlength} !"
+                )
+                self.accumulate_error(err_msg, params)  # type: ignore[attr-defined]
