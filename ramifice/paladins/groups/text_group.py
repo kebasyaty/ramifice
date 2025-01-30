effcfb01bc9a2ef53bcb0ea07075a03c68bd7448
kebasyaty <kebasyaty@gmail.com>
2025-01-30 10:04:31 +0200

    update docstring

--- ramifice/paladins/groups/text_group.py
+++ ramifice/paladins/groups/text_group.py
@@ -1,10 +1,18 @@
-"""Group for checking text fields."""
+"""Group for checking text fields.
+Supported fields:
+URLField | TextField | PhoneField
+| IPField | EmailField | ColorField
+"""
 
 from typing import Any
 
 
 class TextGroupMixin:
-    """Group for checking text fields."""
+    """Group for checking text fields.
+    Supported fields:
+    URLField | TextField | PhoneField
+    | IPField | EmailField | ColorField
+    """
 
     def text_group(self, params: dict[str, Any]) -> None:
         """Checking text fields."""
