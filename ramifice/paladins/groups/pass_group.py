a650bb2ba35a77431a7a2a8b9d8569b29c193c2c
kebasyaty <kebasyaty@gmail.com>
2025-02-02 11:56:27 +0200

    update PassGroupMixin

--- ramifice/paladins/groups/pass_group.py
+++ ramifice/paladins/groups/pass_group.py
@@ -2,6 +2,7 @@
 Supported fields: PasswordField
 """
 
+import hashlib
 from typing import Any
 
 
@@ -12,3 +13,27 @@ class PassGroupMixin:
 
     def pass_group(self, params: dict[str, Any]) -> None:
         """Checking password fields."""
+        field = params["field_data"]
+        # When updating the document, skip the verification.
+        if params["is_update"]:
+            params["field_data"].value = None
+            return
+        # Get current value.
+        value = field.value or field.default
+        if value is None:
+            if field.required:
+                err_msg = "Required field !"
+                self.accumulate_error(err_msg, params)  # type: ignore[attr-defined]
+            if params["is_save"]:
+                params["result_map"] = None
+            return
+        # Validation Passwor.
+        if not field.is_valid(value):
+            err_msg = "Invalid Passwor !"
+            self.accumulate_error(err_msg, params)  # type: ignore[attr-defined]
+        # Insert result.
+        if params["is_save"]:
+            password = value.encode("utf-8")
+            _hash = hashlib.sha256(password)
+            _hash.update(password)
+            params["result_map"][field.name] = _hash.digest()
