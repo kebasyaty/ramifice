192285d381e5df5f14e2605764be72bfb30b88cd
kebasyaty <kebasyaty@gmail.com>
2025-02-03 18:45:00 +0200

    update DateGroupMixin

--- ramifice/paladins/groups/date_group.py
+++ ramifice/paladins/groups/date_group.py
@@ -5,6 +5,9 @@ DateTimeField | DateField
 
 from typing import Any
 
+from ...errors import InvalidDateError, InvalidDateTimeError
+from ...tools import date_parse, datetime_parse
+
 
 class DateGroupMixin:
     """Group for checking date fields.
@@ -15,7 +18,6 @@ class DateGroupMixin:
     def date_group(self, params: dict[str, Any]) -> None:
         """Checking date fields."""
         field = params["field_data"]
-        time_object_list = self.__class__.META["time_object_list"]  # type: ignore[attr-defined]
         # Get current value.
         value = field.value or field.default or None
         if value is None:
@@ -25,3 +27,18 @@ class DateGroupMixin:
             if params["is_save"]:
                 params["result_map"][field.name] = None
             return
+        #
+        if "DateTime" in field.field_type:
+            try:
+                value = datetime_parse(value)
+            except InvalidDateTimeError:
+                err_msg = "Invalid date and time !"
+                self.accumulate_error(err_msg, params)  # type: ignore[attr-defined]
+        else:
+            try:
+                value = date_parse(value)
+            except InvalidDateError:
+                err_msg = "Invalid date !"
+                self.accumulate_error(err_msg, params)  # type: ignore[attr-defined]
+        #
+        time_object_list = self.__class__.META["time_object_list"]  # type: ignore[attr-defined]
