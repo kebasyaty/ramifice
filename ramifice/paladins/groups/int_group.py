6324d5599b50b47814f786c127e4fa4f1367d785
kebasyaty <kebasyaty@gmail.com>
2025-02-03 09:10:01 +0200

    update IntGroupMixin and fix TextGroupMixin

--- ramifice/paladins/groups/int_group.py
+++ ramifice/paladins/groups/int_group.py
@@ -12,3 +12,35 @@ class IntGroupMixin:
 
     def int_group(self, params: dict[str, Any]) -> None:
         """Checking integer fields."""
+        field = params["field_data"]
+        # Get current value.
+        value: int | None = field.value or field.default
+        if value is None:
+            if field.required:
+                err_msg = "Required field !"
+                self.accumulate_error(err_msg, params)  # type: ignore[attr-defined]
+            if params["is_save"]:
+                params["result_map"][field.name] = None
+            return
+        value = int(value)
+        # Validation the `max_number` field attribute.
+        max_number = field.__dict__["max_number"]
+        if max_number is not None and value > max_number:
+            err_msg = (
+                f"The value {value} must not be greater than max_number={max_number} !"
+            )
+            self.accumulate_error(err_msg, params)  # type: ignore[attr-defined]
+        # Validation the `min_number` field attribute.
+        min_number = field.__dict__["min_number"]
+        if min_number is not None and value < min_number:
+            err_msg = (
+                f"The value {value} must not be less than min_number={min_number} !"
+            )
+            self.accumulate_error(err_msg, params)  # type: ignore[attr-defined]
+        # Validation the `unique` field attribute.
+        if field.unique and not self.check_uniqueness(value, params):  # type: ignore[attr-defined]
+            err_msg = "Is not unique !"
+            self.accumulate_error(err_msg, params)  # type: ignore[attr-defined]
+        # Insert result.
+        if params["is_save"]:
+            params["result_map"][field.name] = value
