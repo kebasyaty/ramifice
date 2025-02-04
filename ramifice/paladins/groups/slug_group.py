fba4258ef70d50862f1f21a6c0d7fd7411070a70
kebasyaty <kebasyaty@gmail.com>
2025-02-04 10:26:56 +0200

    update SlugGroupMixin

--- ramifice/paladins/groups/slug_group.py
+++ ramifice/paladins/groups/slug_group.py
@@ -6,6 +6,8 @@ from typing import Any
 
 from slugify import slugify
 
+from ...errors import PanicError
+
 
 class SlugGroupMixin:
     """Group for checking slug fields.
@@ -17,3 +19,26 @@ class SlugGroupMixin:
         field = params["field_data"]
         raw_str_list: list[str] = []
         slug_sources = field.slug_sources
+        #
+        for field_name, field_data in self.__dict__.items():
+            if callable(field_data):
+                continue
+            if field_name in slug_sources:
+                value = field.value
+                if value is None:
+                    value = field.default
+                if value is not None:
+                    raw_str_list.append(value)
+                else:
+                    err_msg = (
+                        f"Model: `{self.full_model_name()}` > "  # type: ignore[attr-defined]
+                        + f"Field: `{field.name}` => "
+                        + f"{field_name} - "
+                        + "This field is specified in slug_sources. "
+                        + "This field should be mandatory or assign a default value."
+                    )
+                    raise PanicError(err_msg)
+        # Insert result.
+        if params["is_save"]:
+            value = "-".join(raw_str_list)
+            params["result_map"][field.name] = slugify(value)
