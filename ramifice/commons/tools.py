5acea850b83ab715dc3100ef59008e8b7b8102a0
kebasyaty <kebasyaty@gmail.com>
2025-05-02 18:18:44 +0300

    update from_doc in Commons

--- ramifice/commons/tools.py
+++ ramifice/commons/tools.py
@@ -1,6 +1,9 @@
 """Tools - A set of additional auxiliary methods for Commons."""
 
+from datetime import datetime
 from typing import Any
+
+from ..types import FileData, ImageData
 
 
 class ToolsMixin:
@@ -11,5 +14,21 @@ class ToolsMixin:
         """Convert Mongo document to a object instance."""
         obj = cls()
         for name, data in mongo_doc.items():
-            obj.__dict__[name] = data
+            if data is None:
+                continue
+            field = obj.__dict__[name]
+            if field.group != "pass":
+                if name != "_id":
+                    if field.group == "date":
+                        if field.input_type == "date":
+                            data = data.strftime("%Y-%m-%d")
+                        else:
+                            data = data.strftime("%Y-%m-%dT%H:%M:%S")
+                    elif field.group == "file":
+                        data = FileData.from_doc(data)
+                    elif field.group == "img":
+                        data = ImageData.from_doc(data)
+                else:
+                    data = str(data)
+                field.value = data
         return obj
