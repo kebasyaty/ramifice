a687b0b9834f58f0723f64e980f140e4c6550a3f
kebasyaty <kebasyaty@gmail.com>
2025-05-03 11:54:48 +0300

    add save methos to Paladins

--- ramifice/paladins/save.py
+++ ramifice/paladins/save.py
@@ -5,3 +5,9 @@ async def save(self) -> bool:
     """Create or update document in database.
     This method pre-uses the `check` method.
     """
+    if self.__class__.META["is_migrat_model"]:
+        pass
+
+    #
+    # If everything is completed successfully.
+    return True
