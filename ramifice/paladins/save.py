9b90ca6689f542399f4dffdd01c96c152b4f8adc
kebasyaty <kebasyaty@gmail.com>
2025-05-03 13:31:07 +0300

    update SaveMixin

--- ramifice/paladins/save.py
+++ ramifice/paladins/save.py
@@ -1,5 +1,7 @@
 """Create or update document in database."""
 
+from datetime import datetime
+
 from .. import store
 from ..errors import PanicError
 
@@ -22,6 +24,37 @@ class SaveMixin:
         collection = store.MONGO_DATABASE[self.__class__.META["collection_name"]]  # type: ignore[index, attr-defined]
         # Check and get ResultCheck.
         result_check = self.check(is_save=True)  # type: ignore[attr-defined]
+        # Reset the alerts to exclude duplicates.
+        self.hash.alerts = []  # type: ignore[index, attr-defined]
+        # Check the conditions and, if necessary, define a message for the web form.
+        if not result_check.is_update and not self.__class__.META["is_create_doc"]:  # type: ignore[index, attr-defined]
+            self.hash.alerts.append("It is forbidden to create new documents !")  # type: ignore[index, attr-defined]
+            result_check.is_valid = False
+        if result_check.is_update and not self.__class__.META["is_update_doc"]:  # type: ignore[index, attr-defined]
+            self.hash.alerts.append("It is forbidden to update documents !")  # type: ignore[index, attr-defined]
+            result_check.is_valid = False
+        # Leave the method if the check fails.
+        if not result_check.is_valid:
+            self.ignored_fields_to_none()  # type: ignore[index, attr-defined]
+            return False
+        # Get data for document.
+        checked_data = result_check.data
+        # Create or update a document in database.
+        if result_check.is_update:
+            # Update date and time.
+            checked_data["updated_at"] = datetime.now()
+            # Run hook.
+            self.pre_update()  # type: ignore[index, attr-defined]
+            # Update doc.
+            mongo_doc = await collection.find_one({"_id": checked_data["_id"]})
+            if mongo_doc is not None:
+                collection.find_one({"_id": checked_data["_id"]}, checked_data)
+            # Run hook.
+            self.post_update()  # type: ignore[index, attr-defined]
+            # Refresh Model.
+            self.update_from_doc(mongo_doc)  # type: ignore[index, attr-defined]
+        else:
+            pass
         #
         # If everything is completed successfully.
         return True
