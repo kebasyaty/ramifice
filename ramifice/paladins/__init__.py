45cc5f449f9eac040df2e7e33440a2e5b6a2a3cc
kebasyaty <kebasyaty@gmail.com>
2025-05-03 12:32:26 +0300

    add SaveMixin to Paladins

--- ramifice/paladins/__init__.py
+++ ramifice/paladins/__init__.py
@@ -1,10 +1,11 @@
 """Paladins - Model instance methods."""
 
 from .check import CheckMixin
+from .save import SaveMixin
 from .tools import ToolsMixin
 
 
-class Paladins(ToolsMixin, CheckMixin):
+class Paladins(ToolsMixin, CheckMixin, SaveMixin):
     """Paladins - Model instance methods."""
 
     def __init__(self):
--- ramifice/paladins/save.py
+++ ramifice/paladins/save.py
@@ -1,20 +1,27 @@
 """Create or update document in database."""
 
+from .. import store
 from ..errors import PanicError
 
 
-async def save(self) -> bool:
-    """Create or update document in database.
-    This method pre-uses the `check` method.
-    """
-    if not self.__class__.META["is_migrat_model"]:
-        msg = (
-            f"Model: `{self.full_model_name()}` > "
-            + "Param: `is_migrat_model` (False) => "
-            + "This Model is not migrated to database!"
-        )
-        raise PanicError(msg)
+class SaveMixin:
+    """Create or update document in database."""
 
-    #
-    # If everything is completed successfully.
-    return True
+    async def save(self) -> bool:
+        """Create or update document in database.
+        This method pre-uses the `check` method.
+        """
+        if not self.__class__.META["is_migrat_model"]:  # type: ignore[attr-defined]
+            msg = (
+                f"Model: `{self.full_model_name()}` > "  # type: ignore[attr-defined]
+                + "Param: `is_migrat_model` (False) => "
+                + "This Model is not migrated to database!"
+            )
+            raise PanicError(msg)
+        # Get collection.
+        collection = store.MONGO_DATABASE[self.__class__.META["collection_name"]]  # type: ignore[index, attr-defined]
+        # Check and get ResultCheck.
+        result_check = self.check(is_save=True)  # type: ignore[attr-defined]
+        #
+        # If everything is completed successfully.
+        return True
