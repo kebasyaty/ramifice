4a9aa284976bb2ea7e81cd23e67e0a347beca416
kebasyaty <kebasyaty@gmail.com>
2025-05-03 12:06:40 +0300

    update save method

--- ramifice/paladins/save.py
+++ ramifice/paladins/save.py
@@ -1,12 +1,19 @@
 """Create or update document in database."""
 
+from ..errors import PanicError
+
 
 async def save(self) -> bool:
     """Create or update document in database.
     This method pre-uses the `check` method.
     """
-    if self.__class__.META["is_migrat_model"]:
-        pass
+    if not self.__class__.META["is_migrat_model"]:
+        msg = (
+            f"Model: `{self.full_model_name()}` > "
+            + "Param: `is_migrat_model` (False) => "
+            + "This Model is not migrated to database!"
+        )
+        raise PanicError(msg)
 
     #
     # If everything is completed successfully.
