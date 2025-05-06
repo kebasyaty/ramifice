cba7cf68415885d332e545c320d634791596ef3c
kebasyaty <kebasyaty@gmail.com>
2025-05-06 15:11:02 +0300

    add distinct method

--- ramifice/commons/general.py
+++ ramifice/commons/general.py
@@ -1,5 +1,7 @@
 """General purpose query methods."""
 
+from typing import Any
+
 from pymongo.asynchronous.collection import AsyncCollection
 from pymongo.asynchronous.command_cursor import AsyncCommandCursor
 
@@ -44,4 +46,25 @@ class GeneralMixin:
         # Get document count.
         return await collection.aggregate(
             pipeline=pipeline, session=session, let=let, comment=comment, **kwargs
+        )
+
+    @classmethod
+    async def distinct(
+        cls, key, filter=None, session=None, comment=None, hint=None, **kwargs
+    ) -> list[Any]:
+        """Finds the distinct values for a specified field across a single collection.
+        Returns an array of unique values for specified field of collection.
+        """
+        # Check if this model is migrated to database.
+        model_is_migrated(cls)
+        # Get collection for current model.
+        collection: AsyncCollection = store.MONGO_DATABASE[cls.META["collection_name"]]  # type: ignore[index, attr-defined]
+        # Get document count.
+        return await collection.distinct(
+            key=key,
+            filter=filter,
+            session=session,
+            comment=comment,
+            hint=hint,
+            **kwargs
         )
