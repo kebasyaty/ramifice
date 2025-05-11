abcf6cbaa496ca79cba8c6ae43d10566930680ba
kebasyaty <kebasyaty@gmail.com>
2025-05-11 13:48:26 +0300

    add find_one_as_doc method

--- ramifice/commons/one.py
+++ ramifice/commons/one.py
@@ -12,8 +12,19 @@ class OneMixin:
     """Requests like `find one`."""
 
     @classmethod
+    async def find_one_as_doc(cls, filter=None, *args, **kwargs) -> Any | None:
+        """Find document."""
+        # Check if this model is migrated to database.
+        model_is_migrated(cls)
+        # Get collection for current model.
+        collection: AsyncCollection = store.MONGO_DATABASE[cls.META["collection_name"]]  # type: ignore[index, attr-defined]
+        # Get document.
+        mongo_doc = await collection.find_one(filter, *args, **kwargs)
+        return mongo_doc
+
+    @classmethod
     async def find_one_to_instance(cls, filter=None, *args, **kwargs) -> Any | None:
-        """Finds the document and converts it to a Model instance."""
+        """Find document and convert it to a Model instance."""
         # Check if this model is migrated to database.
         model_is_migrated(cls)
         # Get collection for current model.
