aa5550b3bda7bc51fd1b7f14d1de58f21daa1cd1
kebasyaty <kebasyaty@gmail.com>
2025-05-11 13:36:43 +0300

    updatw find_one_to_instance method

--- ramifice/commons/one.py
+++ ramifice/commons/one.py
@@ -12,17 +12,16 @@ class OneMixin:
     """Requests like `find one`."""
 
     @classmethod
-    async def find_one_to_instance(
-        cls, filter=None, *args, **kwargs
-    ) -> dict[str, Any] | None:
+    async def find_one_to_instance(cls, filter=None, *args, **kwargs) -> Any | None:
         """Finds the document and converts it to a Model instance."""
         # Check if this model is migrated to database.
         model_is_migrated(cls)
         # Get collection for current model.
         collection: AsyncCollection = store.MONGO_DATABASE[cls.META["collection_name"]]  # type: ignore[index, attr-defined]
         # Get document.
-        mongo_doc = await collection.find_one(filter, *args, **kwargs)
         inst_model = None
+        mongo_doc = await collection.find_one(filter, *args, **kwargs)
         if mongo_doc is not None:
+            # Convert document to Model instance.
             inst_model = cls.from_doc(mongo_doc)  # type: ignore[index, attr-defined]
         return inst_model
