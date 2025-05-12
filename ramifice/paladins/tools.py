32fc7ee3b29081e3dce143153e560afc26d897ae
kebasyaty <kebasyaty@gmail.com>
2025-05-12 15:16:53 +0300

    update ignored_fields_to_none method

--- ramifice/paladins/tools.py
+++ ramifice/paladins/tools.py
@@ -76,7 +76,7 @@ class ToolMixin:
         return await params["collection"].find_one(q_filter) is None
 
     def ignored_fields_to_none(self):
-        """Reset the values ​​of ignored fields to None."""
+        """Reset the values of ignored fields to None."""
         for _, field_data in self.__dict__.items():
             if (
                 not callable(field_data)
@@ -88,10 +88,8 @@ class ToolMixin:
     def update_from_doc(self, mongo_doc: dict[str, Any]):
         """Update object instance from Mongo document."""
         for name, data in mongo_doc.items():
-            if data is None:
-                continue
-            if name == "_id":
+            if name != "_id":
+                field = self.__dict__[name]
+                field.value = data if field.group != "pass" else None
+            else:
                 self.__dict__["hash"].value = str(data)
-                continue
-            field = self.__dict__[name]
-            field.value = data if field.group != "pass" else None
