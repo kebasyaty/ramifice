25fceeebb6c07aa67ac989ec61baaf18aba25068
kebasyaty <kebasyaty@gmail.com>
2025-05-18 09:28:09 +0300

    add mongo_doc_to_model_doc method

--- ramifice/commons/many.py
+++ ramifice/commons/many.py
@@ -3,11 +3,75 @@
 from typing import Any
 
 from pymongo.asynchronous.collection import AsyncCollection
+from pymongo.asynchronous.cursor import AsyncCursor, CursorType
 from pymongo.results import DeleteResult
 
 from .. import store
+from ..errors import PanicError
 from ..tools import model_is_migrated
 
 
 class ManyMixin:
     """Queries like `find many`."""
+
+
+async def find_many(
+    cls,
+    filter=None,
+    projection=None,
+    skip=0,
+    limit=0,
+    no_cursor_timeout=False,
+    cursor_type=CursorType.NON_TAILABLE,
+    sort=None,
+    allow_partial_results=False,
+    oplog_replay=False,
+    batch_size=0,
+    collation=None,
+    hint=None,
+    max_scan=None,
+    max_time_ms=None,
+    max=None,
+    min=None,
+    return_key=False,
+    show_record_id=False,
+    snapshot=False,
+    comment=None,
+    session=None,
+    allow_disk_use=None,
+) -> list[dict[str, Any]]:
+    """Find documents matching with Model."""
+    # Check if this model is migrated to database.
+    model_is_migrated(cls)
+    # Get collection for current model.
+    collection: AsyncCollection = store.MONGO_DATABASE[cls.META["collection_name"]]  # type: ignore[index, attr-defined]
+    # Get documents.
+    doc_list: list[dict[str, Any]] = []
+    cursor: AsyncCursor = collection.find(
+        filter=filter,
+        projection=projection,
+        skip=skip,
+        limit=limit or cls.META["db_query_docs_limit"],
+        no_cursor_timeout=no_cursor_timeout,
+        cursor_type=cursor_type,
+        sort=sort,
+        allow_partial_results=allow_partial_results,
+        oplog_replay=oplog_replay,
+        batch_size=batch_size,
+        collation=collation,
+        hint=hint,
+        max_scan=max_scan,
+        max_time_ms=max_time_ms,
+        max=max,
+        min=min,
+        return_key=return_key,
+        show_record_id=show_record_id,
+        snapshot=snapshot,
+        comment=comment,
+        session=session,
+        allow_disk_use=allow_disk_use,
+    )
+    async for mongo_doc in cursor:
+        doc_list.append(cls.mongo_doc_to_model_doc(mongo_doc))
+
+    return doc_list
--- ramifice/commons/tools.py
+++ ramifice/commons/tools.py
@@ -2,6 +2,8 @@
 
 from typing import Any
 
+from ..tools import date_parse, datetime_parse
+
 
 class ToolMixin:
     """A set of additional auxiliary methods for Commons."""
@@ -13,5 +15,23 @@ class ToolMixin:
         for name, data in mongo_doc.items():
             field = obj.__dict__[name]
             field.value = data if field.group != "pass" else None
-
         return obj
+
+    @classmethod
+    def mongo_doc_to_model_doc(cls, mongo_doc: dict[str, Any]) -> dict[str, Any]:
+        """Convert the Mongo document to the Model document."""
+        doc: dict[str, Any] = {}
+        for f_name, t_name in cls.META["field_name_and_type"].items():  # type: ignore[index, attr-defined]
+            value = mongo_doc[f_name]
+            if value is not None:
+                if "Date" in t_name:
+                    if "Time" in t_name:
+                        value = value.strftime("%Y-%m-%d %H:%M:%S")
+                    else:
+                        value = value.strftime("%Y-%m-%d")
+                elif "Hash" in t_name:
+                    value = str(value)
+                elif "Pass" in t_name:
+                    value = None
+            doc[f_name] = value
+        return doc
