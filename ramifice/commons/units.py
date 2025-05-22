c87925b9e65dbdd2a3140c0628a66c03779f9c2c
kebasyaty <kebasyaty@gmail.com>
2025-05-22 12:33:17 +0300

    fix unit_manager method

--- ramifice/commons/units.py
+++ ramifice/commons/units.py
@@ -16,11 +16,3 @@ class UnitMixin:
         Management for `choices` parameter in dynamic field types.
         """
         cls_model = self.__class__
-        # Check if this model is migrated to database.
-        if not cls_model.META["is_migrat_model"]:  # type: ignore[index, attr-defined]
-            msg = (
-                f"Model: `{cls_model.META["full_model_name"]}` > "  # type: ignore[index, attr-defined]
-                + "META param: `is_migrat_model` (False) => "
-                + "This Model is not migrated to database!"
-            )
-            raise PanicError(msg)
