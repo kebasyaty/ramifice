56ef7357e5c7f1eb2c5879198340f45fddc35d57
kebasyaty <kebasyaty@gmail.com>
2025-05-22 12:19:49 +0300

    add unit_manager method

--- ramifice/commons/units.py
+++ ramifice/commons/units.py
@@ -2,8 +2,16 @@
 Management for `choices` parameter in dynamic field types.
 """
 
+from ..types import Unit
+
 
 class UnitMixin:
     """Units Management.
     Management for `choices` parameter in dynamic field types.
     """
+
+    async def unit_manager(self, unit: Unit) -> None:
+        """Units Management.
+        Management for `choices` parameter in dynamic field types.
+        """
+        pass
