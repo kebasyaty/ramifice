85e053affb0e4b86541c0ee5c7329feff02b8355
kebasyaty <kebasyaty@gmail.com>
2025-05-23 10:43:25 +0300

    update class Unit

--- ramifice/types.py
+++ ramifice/types.py
@@ -30,13 +30,10 @@ class Unit(JsonMixin):
         self.value = value
         self.is_delete = is_delete
 
-        self.is_valid()
+        self.error_empty_field()
 
-    def is_valid(self) -> None:
-        """Unit validation.
-        Raises panic if it finds inconsistencies.
-        """
-        # Error: If any of the fields in the Unit is empty.
+    def error_empty_field(self) -> None:
+        """Error: If any of the fields in the Unit is empty."""
         field_name: str = ""
 
         if len(self.field) == 0:
