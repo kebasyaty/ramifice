9c806b57c445e7bf004ba514cd907464faa219ce
kebasyaty <kebasyaty@gmail.com>
2025-05-23 10:33:45 +0300

    uodate unit_manager method

--- ramifice/commons/units.py
+++ ramifice/commons/units.py
@@ -20,6 +20,9 @@ class UnitMixin:
         Management for `choices` parameter in dynamic field types.
         """
         cls_model = self.__class__
+        # Unit validation.
+        # Raises panic if it finds inconsistencies.
+        unit.is_valid(model_state)
         # Get access to super collection.
         # (Contains Model state and dynamic field data.)
         super_collection: AsyncCollection = store.MONGO_DATABASE[  # type: ignore[index]
@@ -29,7 +32,4 @@ class UnitMixin:
         model_state: dict[str, Any] | None = await super_collection.find_one(
             filter={"collection_name": cls_model.META["collection_name"]}  # type: ignore[attr-defined]
         )
-        # Unit validation.
-        # Raises panic if it finds inconsistencies.
-        unit.is_valid(model_state)
         # Check the presence of a dynamic field.
