cbc0f550f41e228fd95d597637abd276bd09d28c
kebasyaty <kebasyaty@gmail.com>
2025-05-26 09:07:52 +0300

    update test_unit

--- tests/commons/test_unit.py
+++ tests/commons/test_unit.py
@@ -6,34 +6,12 @@ from pymongo import AsyncMongoClient
 
 from ramifice import model, store
 from ramifice.fields import (
-    BooleanField,
     ChoiceFloatDynField,
-    ChoiceFloatField,
     ChoiceFloatMultDynField,
-    ChoiceFloatMultField,
     ChoiceIntDynField,
-    ChoiceIntField,
     ChoiceIntMultDynField,
-    ChoiceIntMultField,
     ChoiceTextDynField,
-    ChoiceTextField,
     ChoiceTextMultDynField,
-    ChoiceTextMultField,
-    ColorField,
-    DateField,
-    DateTimeField,
-    EmailField,
-    FileField,
-    FloatField,
-    HashField,
-    ImageField,
-    IntegerField,
-    IPField,
-    PasswordField,
-    PhoneField,
-    SlugField,
-    TextField,
-    URLField,
 )
 from ramifice.migration import Monitor
 
@@ -43,43 +21,21 @@ class User:
     """Model for testing."""
 
     def fields(self):
-        self.url = URLField()
-        self.txt = TextField()
-        self.slug = SlugField()
-        self.phone = PhoneField()
-        self.password = PasswordField()
-        self.ip = IPField()
-        self.num_int = IntegerField()
-        self.num_float = FloatField()
-        self.img = ImageField()
-        self.hash2 = HashField()
-        self.file = FileField()
-        self.email = EmailField()
-        self.date_time = DateTimeField()
-        self.date = DateField()
-        self.color = ColorField()
-        self.bool = BooleanField()
         self.choice_float_dyn = ChoiceFloatDynField()
-        self.choice_float = ChoiceFloatField()
         self.choice_float_mult_dyn = ChoiceFloatMultDynField()
-        self.choice_float_mult = ChoiceFloatMultField()
         self.choice_int_dyn = ChoiceIntDynField()
         self.choice_int_mult_dyn = ChoiceIntMultDynField()
-        self.choice_int_mult = ChoiceIntMultField()
         self.choice_txt_dyn = ChoiceTextDynField()
-        self.choice_txt = ChoiceTextField()
         self.choice_txt_mult_dyn = ChoiceTextMultDynField()
-        self.choice_txt_mult = ChoiceTextMultField()
-        self.choice_int = ChoiceIntField()
 
 
-class TestCommonOneMixin(unittest.IsolatedAsyncioTestCase):
-    """Testing `Ramifice > QCommonsMixin > OneMixin` module."""
+class TestCommonUnitMixin(unittest.IsolatedAsyncioTestCase):
+    """Testing `Ramifice > QCommonsMixin > UnitMixin` module."""
 
-    async def test_one_mixin_methods(self):
+    async def test_unit_mixin_methods(self):
         """Testing OneMixin methods."""
         # Maximum number of characters 60.
-        database_name = "test_one_mixin_methods"
+        database_name = "test_unit_mixin_methods"
 
         # Delete database before test.
         # (if the test fails)
