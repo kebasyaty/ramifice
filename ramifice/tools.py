a7c4183af0b9c7d8637c507e434a2e24030d6ae4
kebasyaty <kebasyaty@gmail.com>
2025-05-28 17:41:41 +0300

    add block caomments for date_parse and datetime_parse methods

--- ramifice/tools.py
+++ ramifice/tools.py
@@ -3,7 +3,8 @@
 import ipaddress
 import math
 import os
-from datetime import datetime
+
+# from datetime import datetime
 from typing import Any
 from urllib.parse import urlparse
 
@@ -11,41 +12,40 @@ import phonenumbers
 from bson.objectid import ObjectId
 from email_validator import EmailNotValidError, validate_email
 
-from .errors import InvalidDateError, InvalidDateTimeError
+# from .errors import InvalidDateError, InvalidDateTimeError
 from .store import REGEX
 
+# def date_parse(date: str) -> datetime:
+#     """Converting a string with Date to object `datetime`.
+#     Formats: dd-mm-yyyy | dd/mm/yyyy | dd.mm.yyyy |
+#              yyyy-mm-dd | yyyy/mm/dd | yyyy.mm.dd
+#     """
+#     md = REGEX["date_parse"].match(date) or REGEX["date_parse_reverse"].match(date)
+#     if md is None:
+#         raise InvalidDateError()
+#     dt = datetime.strptime(
+#         f"{md.group('y')}-{md.group('m')}-{md.group('d')}", "%Y-%m-%d"
+#     )
+#     return dt
 
-def date_parse(date: str) -> datetime:
-    """Converting a string with Date to object `datetime`.
-    Formats: dd-mm-yyyy | dd/mm/yyyy | dd.mm.yyyy |
-             yyyy-mm-dd | yyyy/mm/dd | yyyy.mm.dd
-    """
-    md = REGEX["date_parse"].match(date) or REGEX["date_parse_reverse"].match(date)
-    if md is None:
-        raise InvalidDateError()
-    dt = datetime.strptime(
-        f"{md.group('y')}-{md.group('m')}-{md.group('d')}", "%Y-%m-%d"
-    )
-    return dt
 
-
-def datetime_parse(date_time: str) -> datetime:
-    """Converting a string with Date and Time to the object `datetime`.
-    Formats: dd-mm-yyyy hh:mm:ss | dd/mm/yyyy hh:mm:ss | dd.mm.yyyy hh:mm:ss |
-             dd-mm-yyyyThh:mm:ss | dd/mm/yyyyThh:mm:ss | dd.mm.yyyyThh:mm:ss |
-             yyyy-mm-dd hh:mm:ss | yyyy/mm/dd hh:mm:ss | yyyy.mm.dd hh:mm:ss |
-             yyyy-mm-ddThh:mm:ss | yyyy/mm/ddThh:mm:ss | yyyy.mm.ddThh:mm:ss
-    """
-    md = REGEX["datetime_parse"].match(date_time) or REGEX[
-        "datetime_parse_reverse"
-    ].match(date_time)
-    if md is None:
-        raise InvalidDateTimeError()
-    dt = datetime.strptime(
-        f"{md.group('y')}-{md.group('m')}-{md.group('d')}T{md.group('t')}",
-        "%Y-%m-%dT%H:%M:%S",
-    )
-    return dt
+# def datetime_parse(date_time: str) -> datetime:
+#     """Converting a string with Date and Time to the object `datetime`.
+#     Formats: dd-mm-yyyy hh:mm:ss | dd/mm/yyyy hh:mm:ss | dd.mm.yyyy hh:mm:ss |
+#              dd-mm-yyyyThh:mm:ss | dd/mm/yyyyThh:mm:ss | dd.mm.yyyyThh:mm:ss |
+#              yyyy-mm-dd hh:mm:ss | yyyy/mm/dd hh:mm:ss | yyyy.mm.dd hh:mm:ss |
+#              yyyy-mm-ddThh:mm:ss | yyyy/mm/ddThh:mm:ss | yyyy.mm.ddThh:mm:ss
+#     """
+#     md = REGEX["datetime_parse"].match(date_time) or REGEX[
+#         "datetime_parse_reverse"
+#     ].match(date_time)
+#     if md is None:
+#         raise InvalidDateTimeError()
+#     dt = datetime.strptime(
+#         f"{md.group('y')}-{md.group('m')}-{md.group('d')}T{md.group('t')}",
+#         "%Y-%m-%dT%H:%M:%S",
+#     )
+#     return dt
 
 
 def to_human_size(size: int) -> str:
