a5fbb36ba696af932f3de8a6c08705642da71217
kebasyaty <kebasyaty@gmail.com>
2025-05-31 10:46:39 +0300

    fix find_one_to_json method

--- ramifice/commons/one.py
+++ ramifice/commons/one.py
@@ -45,7 +45,6 @@ class OneMixin:
         json_str: str | None = None
         mongo_doc = await collection.find_one(filter, *args, **kwargs)
         if mongo_doc is not None:
-            mongo_doc = cls.password_to_none(mongo_doc)
             # Convert document to Model instance.
             inst_model = cls.from_doc(mongo_doc)  # type: ignore[index, attr-defined]
             json_str = inst_model.to_json()
