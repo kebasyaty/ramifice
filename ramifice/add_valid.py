4a0850e505da201ad1b8888cb6f3dda2a3488d36
kebasyaty <kebasyaty@gmail.com>
2025-06-01 16:10:14 +0300

    fix docstring in add_validation method

--- ramifice/add_valid.py
+++ ramifice/add_valid.py
@@ -7,7 +7,7 @@ class AddValidMixin(metaclass=ABCMeta):
     """Additional validation of fields."""
 
     async def add_validation(self) -> dict[str, str]:
-        """It is supposed to be use to additional validation of fields.
+        """For additional validation of fields.
         Format: <"field_name", "Error message">
         """
         error_map: dict[str, str] = {}
