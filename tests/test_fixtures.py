14d5d32da1d5413603ca657a604f6873d65b6528
kebasyaty <kebasyaty@gmail.com>
2025-06-03 14:23:20 +0300

    update test_fixtures

--- tests/test_fixtures.py
+++ tests/test_fixtures.py
@@ -51,9 +51,7 @@ class User:
         self.ip = IPField()
         self.num_int = IntegerField()
         self.num_float = FloatField()
-        self.img = ImageField(
-            thumbnails={"lg": 480, "md": 240, "sm": 120, "xs": 60},
-        )
+        self.img = ImageField()
         self.hash2 = IDField()
         self.file = FileField()
         self.email = EmailField()
