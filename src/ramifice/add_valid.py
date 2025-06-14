cb8091bd7133ab2172812cbc0d23960aea17b552
kebasyaty <kebasyaty@gmail.com>
2025-06-14 18:38:36 +0300

    Update docstring for AddValidMixin

--- src/ramifice/add_valid.py
+++ src/ramifice/add_valid.py
@@ -7,6 +7,6 @@ class AddValidMixin(metaclass=ABCMeta):
     """Additional validation of fields."""
 
     async def add_validation(self) -> dict[str, str]:
-        """For additional validation of fields."""
+        """Additional validation of fields."""
         error_map: dict[str, str] = {}
         return error_map
