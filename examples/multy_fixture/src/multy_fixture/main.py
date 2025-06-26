c638f25682d830213d3a02e14ba15f1d8129d4cf
kebasyaty <kebasyaty@gmail.com>
2025-06-26 12:10:14 +0300

    add examples/multy_fixture

---
+++ examples/multy_fixture/src/multy_fixture/main.py
@@ -0,0 +1,41 @@
+"""App."""
+
+import asyncio
+import pprint
+
+from pymongo import AsyncMongoClient
+from ramifice import migration, translations
+
+from models.accounts import User
+
+
+async def main() -> None:
+    """Main."""
+    client: AsyncMongoClient = AsyncMongoClient()
+
+    await migration.Monitor(
+        database_name="test_db",
+        mongo_client=client,
+    ).migrat()
+
+    # If you need to change the language of translation.
+    translations.change_locale("en")
+
+    user = await User.find_one_to_instance({"email": "John_Smith@gmail.com"})
+    user2 = await User.find_one_to_instance({"email": "kebasyaty@gmail.com"})
+
+    print("User details:")
+    user_details = await User.find_one_to_raw_doc({"_id": user.id.value})
+    pprint.pprint(user_details)
+    await user.delete(remove_files=False)
+
+    print("\nUser 2 details:")
+    user2_details = await User.find_one_to_raw_doc({"_id": user2.id.value})
+    pprint.pprint(user2_details)
+    await user2.delete(remove_files=False)
+
+    await client.close()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
