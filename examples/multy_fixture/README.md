5f34e425bab18d7e2b9c71dbd0115847f8fae5e9
kebasyaty <kebasyaty@gmail.com>
2025-06-26 18:19:23 +0300

    add examples/multy_fixture

---
+++ examples/multy_fixture/README.md
@@ -0,0 +1,36 @@
+# multy_fixture
+
+**An example of a fixture with several documents.**
+<br>
+**Fixtures:** To populate the database with pre-created data.
+<br>
+**config/fixtures** - Directory for creating fixtures.
+<br>
+<br>
+**Path to file with an example:**
+<br>
+_packages/models/src/models/site.py_
+<br>
+_config/fixtures/SiteParameters.yml_
+<br>
+_src/multy_fixture/main.py_
+
+## Run an example
+
+```shell
+# Fedora:
+sudo dnf install gettext
+gettext --version
+# Ubuntu:
+sudo apt install gettext
+gettext --version
+# Windows:
+https://mlocati.github.io/articles/gettext-iconv-windows.html
+gettext --version
+
+uv python install
+
+cd project_name
+uv sync
+uv run python src/multy_fixture/main.py
+```
---
+++ examples/multy_fixture/src/multy_fixture/main.py
@@ -0,0 +1,51 @@
+"""App."""
+
+import asyncio
+import pprint
+
+from pymongo import AsyncMongoClient
+from ramifice import migration, translations
+
+from models.site import Parameters
+
+
+async def main() -> None:
+    """Main."""
+    client: AsyncMongoClient = AsyncMongoClient()
+
+    await migration.Monitor(
+        database_name="test_db",
+        mongo_client=client,
+    ).migrat()
+
+    # If you need to change the language of translation.
+    # Hint: For Ramifice by default = "en"
+    translations.change_locale("en")
+
+    params = await Parameters.find_one_to_instance(
+        {f"brand.{translations.CURRENT_LOCALE}": "Brand Name"}
+    )
+    if params is not None:
+        print("Details of Parameters:")
+        user_details = await Parameters.find_one_to_raw_doc({"_id": params.id.value})
+        pprint.pprint(user_details)
+        # await params.delete(remove_files=False)
+    else:
+        print("No parameters!")
+
+    params_2 = await Parameters.find_one_to_instance(
+        {f"brand.{translations.CURRENT_LOCALE}": "Brand Name 2"}
+    )
+    if params_2 is not None:
+        print("\nDetails of Parameters:")
+        user_details_2 = await Parameters.find_one_to_raw_doc({"_id": params_2.id.value})
+        pprint.pprint(user_details_2)
+        # await params_2.delete(remove_files=False)
+    else:
+        print("No parameters 2!")
+
+    await client.close()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
