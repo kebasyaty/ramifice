29f1504693ba880dcdd3652a8a9fced51de5cf5a
kebasyaty <kebasyaty@gmail.com>
2025-06-27 12:43:31 +0300

    update examples

--- examples/password/packages/models/src/models/accounts.py
+++ examples/password/packages/models/src/models/accounts.py
@@ -69,6 +69,12 @@ class User:
         """Additional validation of fields."""
         gettext = translations.gettext
         error_map: dict[str, str] = {}
-        if self.id.value is None and (self.password.value != self.сonfirm_password.value):
+
+        # Get clean data.
+        id = self.id.value
+        password = self.password.value
+        сonfirm_password = self.сonfirm_password.value
+
+        if id is None and (password != сonfirm_password):
             error_map["password"] = gettext("Passwords do not match!")
         return error_map
