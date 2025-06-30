5a3a1a05f9ddca98f1634611e6c4b44c2ac15baa
kebasyaty <kebasyaty@gmail.com>
2025-06-30 10:57:56 +0300

    update examples/basic

--- examples/basic/src/basic/main.py
+++ examples/basic/src/basic/main.py
@@ -4,10 +4,10 @@ import asyncio
 import pprint
 from datetime import datetime
 
-from models.accounts import User
 from pymongo import AsyncMongoClient
+from ramifice import migration, translations
 
-from ramifice import migration
+from models.accounts import User
 
 
 async def main() -> None:
@@ -21,14 +21,20 @@ async def main() -> None:
 
     # If you need to change the language of translation.
     # Hint: For Ramifice by default = "en"
-    # translations.change_locale("ru")
+    translations.change_locale("en")
 
     user = User()
+    # user.avatar.from_path("public/media/default/no-photo.png")
     user.username.value = "pythondev"
-    user.first_name.value = "John"
-    user.last_name.value = "Smith"
+    user.first_name.value = {"en": "John", "ru": "Джон"}
+    # user.first_name.value = "John"
+    user.last_name.value = {"en": "Smith", "ru": "Смит"}
+    # user.last_name.value = "Smith"
     user.email.value = "John_Smith@gmail.com"
+    user.phone.value = "+447986123456"
     user.birthday.value = datetime(2000, 1, 25)
+    user.description.value = {"en": "I program on Python!", "ru": "Я программирую на Python!"}
+    # user.description.value = "I program on Python!"
     user.password.value = "12345678"
     user.сonfirm_password.value = "12345678"
 
@@ -38,21 +44,25 @@ async def main() -> None:
         user.print_err()
 
     # Update User.
-    user.username.value = "pythondev-123"
+    user.username.value = "pythondev_123"
     if not await user.save():
         # Convenient to use during development.
         user.print_err()
 
     print("User details:")
-    user_details = await User.find_one_to_raw_doc({"_id": user.id.value})
-    pprint.pprint(user_details)
+    user_details = await User.find_one({"_id": user.id.value})
+    if user_details is not None:
+        pprint.pprint(user_details)
+    else:
+        print("No User!")
 
     # Remove User.
-    await user.delete()
+    # (if necessary)
+    # await user.delete()
 
     # Remove collection.
     # (if necessary)
-    await User.collection().drop()
+    # await User.collection().drop()
 
     await client.close()
 
