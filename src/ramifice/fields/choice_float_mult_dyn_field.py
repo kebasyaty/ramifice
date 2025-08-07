557ac4f9ec66e9f415303a29ca9223a4a55b6dd2
kebasyaty <kebasyaty@gmail.com>
2025-08-07 17:28:04 +0300

    update docstring to ChoiceFloatMultDynField

--- src/ramifice/fields/choice_float_mult_dyn_field.py
+++ src/ramifice/fields/choice_float_mult_dyn_field.py
@@ -20,6 +20,16 @@ class ChoiceFloatMultDynField(Field, ChoiceGroup, JsonMixin):
 
     Type of selective float field with dynamic addition of elements.
     For simulate relationship Many-to-Many.
+
+    Args:
+        label: Text label for a web form field.
+        hide: Hide field from user.
+        disabled: Blocks access and modification of the element.
+        ignored: If true, the value of this field is not saved in the database.
+        hint: An alternative for the `placeholder` parameter.
+        warning: Warning information.
+        required: Required field.
+        readonly: Specifies that the field cannot be modified by the user.
     """
 
     def __init__(  # noqa: D107
