7e167f585a8515fad49780bae3c5dd5e659a9f4a
kebasyaty <kebasyaty@gmail.com>
2025-12-15 13:07:10 +0200

    update comment

--- src/ramifice/__init__.py
+++ src/ramifice/__init__.py
@@ -7,6 +7,10 @@
 #   MM   `Mb. 8M   MM    MM    MM    MM    MM    MM    MM YM.    , YM.    ,
 # .JMML. .JMM.`Moo9^Yo..JMML  JMML  JMML..JMML..JMML..JMML.YMbmd'   `Mbmmd'
 #
+# Copyright (c) 2024 Gennady Kostyunin
+# Ramifice is free software under terms of the MIT License.
+# Repository https://github.com/kebasyaty/ramifice
+#
 #
 # Copyright 2022-present MongoDB, Inc.
 #
