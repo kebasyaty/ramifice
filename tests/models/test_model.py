68ed680d6e5e2663f10cd9b143236a21d2570ff5
develgen <develgen.dev@gmail.com>
2026-07-08 16:50:11 +0300

    update test for model

--- tests/models/test_model.py
+++ tests/models/test_model.py
@@ -5,7 +5,36 @@ from __future__ import annotations
 import unittest
 
 from ramifice import model
-from ramifice.fields import TextField
+from ramifice.fields import (
+    BooleanField,
+    ChoiceFloatDynField,
+    ChoiceFloatField,
+    ChoiceFloatMultDynField,
+    ChoiceFloatMultField,
+    ChoiceIntDynField,
+    ChoiceIntField,
+    ChoiceIntMultDynField,
+    ChoiceIntMultField,
+    ChoiceTextDynField,
+    ChoiceTextField,
+    ChoiceTextMultDynField,
+    ChoiceTextMultField,
+    ColorField,
+    DateField,
+    DateTimeField,
+    EmailField,
+    FileField,
+    FloatField,
+    IDField,
+    ImageField,
+    IntegerField,
+    IPField,
+    PasswordField,
+    PhoneField,
+    SlugField,
+    TextField,
+    URLField,
+)
 from ramifice.models.model import Model
 
 
@@ -13,9 +42,37 @@ from ramifice.models.model import Model
 class User:
     """Model for testing."""
 
-    def fields(self):
+    def fields(self) -> None:
         """Adding fields."""
-        self.username = TextField()
+        self.url = URLField()
+        self.txt = TextField()
+        self.txt2 = TextField()
+        self.slug = SlugField()
+        self.phone = PhoneField()
+        self.password = PasswordField()
+        self.ip = IPField()
+        self.num_int = IntegerField()
+        self.num_float = FloatField()
+        self.img = ImageField()
+        self.hash2 = IDField()
+        self.file = FileField()
+        self.email = EmailField()
+        self.date_time = DateTimeField()
+        self.date = DateField()
+        self.color = ColorField()
+        self.bool = BooleanField()
+        self.choice_float_dyn = ChoiceFloatDynField()
+        self.choice_float = ChoiceFloatField()
+        self.choice_float_mult_dyn = ChoiceFloatMultDynField()
+        self.choice_float_mult = ChoiceFloatMultField()
+        self.choice_int_dyn = ChoiceIntDynField()
+        self.choice_int_mult_dyn = ChoiceIntMultDynField()
+        self.choice_int_mult = ChoiceIntMultField()
+        self.choice_txt_dyn = ChoiceTextDynField()
+        self.choice_txt = ChoiceTextField()
+        self.choice_txt_mult_dyn = ChoiceTextMultDynField()
+        self.choice_txt_mult = ChoiceTextMultField()
+        self.choice_int = ChoiceIntField()
 
 
 class TestModel(unittest.TestCase):
