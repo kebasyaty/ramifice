56e203f64d1523f145f58d97e05274b7ba06168b
develgen <develgen.dev@gmail.com>
2026-07-14 09:28:20 +0300

    update test_json

--- tests/test_json.py
+++ tests/test_json.py
@@ -35,7 +35,6 @@ from ramifice.fields import (
     TextField,
     URLField,
 )
-from ramifice.json import JsonMixin
 
 # For `FileField.value`.
 FILE_INFO_DICT: dict[str, str | int | bool] = dict(
@@ -73,22 +72,6 @@ IMG_INFO_DICT: dict[str, str | int | bool] = dict(
     save_as_is=False,
     ext_upper="",
 )
-
-
-class StandardTypes(JsonMixin):
-    """Class for testing."""
-
-    def __init__(self) -> None:  # noqa: D107
-        JsonMixin.__init__(self)
-        self.x1 = {"x": 1, "y": 2}
-        self.x2 = [1, 2, 3]
-        self.x3 = (1, 2, 3)
-        self.x4 = "Hello world!"
-        self.x5 = 12
-        self.x6 = 12.3
-        self.x7 = True
-        self.x8 = False
-        self.x9 = None
 
 
 @model(service_name="Accounts")
@@ -129,23 +112,8 @@ class User:
 class TestJsonMixin(unittest.TestCase):
     """Testing the class `JsonMixin`."""
 
-    def test_standard_types(self) -> None:
-        """Testing standard types to JSON."""
-        x = StandardTypes()
-        json_str = x.to_json()
-        y = StandardTypes.from_json(json_str)
-        self.assertEqual(x.x1, y.x1)
-        self.assertEqual(x.x2, y.x2)
-        self.assertEqual(x.x3, (1, 2, 3))  # y.x3 == [1, 2, 3]
-        self.assertEqual(x.x4, y.x4)
-        self.assertEqual(x.x5, y.x5)
-        self.assertEqual(x.x6, y.x6)
-        self.assertEqual(x.x7, y.x7)
-        self.assertEqual(x.x8, y.x8)
-        self.assertEqual(x.x9, y.x9)
-
     def test_fields(self) -> None:
-        """Testing the Ramifice fields to JSON."""
+        """Testing all fields."""
         m = User()
 
         json_str = m.to_json()
@@ -162,23 +130,14 @@ class TestJsonMixin(unittest.TestCase):
             if not callable(data):
                 self.assertEqual(m3.__dict__[name].__dict__, data.__dict__)
 
+    def test_file_fields(self) -> None:
+        """Testing file type fields."""
         m = User()
         m.img = IMG_INFO_DICT.copy()
         m.file = FILE_INFO_DICT.copy()
 
         json_str = m.to_json()
         m2 = User.from_json(json_str)
-        for name, data in m.__dict__["img"].__dict__["value"].items():
-            if not callable(data):
-                data2 = m2.__dict__["img"].__dict__["value"][name]
-                self.assertEqual(data, data2)
-        for name, data in m.__dict__["file"].__dict__["value"].items():
-            if not callable(data):
-                data2 = m2.__dict__["file"].__dict__["value"][name]
-                self.assertEqual(data, data2)
-
-        json_str = m.to_json_only_value()
-        m3 = User.from_json_only_value(json_str)
         for name, data in m.__dict__["img"].__dict__["value"].items():
             if not callable(data):
                 data2 = m2.__dict__["img"].__dict__["value"][name]
