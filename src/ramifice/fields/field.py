7af0894bed7e5284d828faff1195a455d87521fe
develgen <develgen.dev@gmail.com>
2026-07-15 07:22:34 +0300

    add `parse` to field.py

--- src/ramifice/fields/field.py
+++ src/ramifice/fields/field.py
@@ -9,6 +9,8 @@ __all__ = ("Field",)
 
 from typing import Any
 
+from dateutil.parser import parse
+
 from ramifice.translations import Translations
 
 
@@ -40,27 +42,34 @@ class Field:
             ]
             msg = f"Not а `{' | '.join(supported_types_list)}` type!"
             raise TypeError(msg)
+
         field_name_html_attrs = self.field_name_html_attrs
+        html_attrs = self.html_attrs
+
         if not hasattr(instance, field_name_html_attrs):
             name = self.name
-            html_attrs = self.html_attrs
             html_attrs["id"] = f"id-{name}"
             html_attrs["name"] = name
-            #
+
             label = html_attrs.get("label")
             html_attrs["label"] = Translations._(label) if bool(label) else ""
-            #
+
             placeholder = html_attrs.get("placeholder")
             if placeholder is not None:
                 html_attrs["placeholder"] = Translations._(placeholder) if bool(placeholder) else ""
-            #
+
             hint = html_attrs.get("hint")
             html_attrs["hint"] = Translations._(hint) if bool(hint) else ""
-            #
+
             warning_list = html_attrs.get("warning")
             if warning_list is not None:
                 html_attrs["warning"] = [Translations._(item) for item in warning_list]
-            #
+
             instance.__dict__[field_name_html_attrs] = html_attrs
-        instance.__dict__[self.internal_name] = value
-        instance.__dict__[field_name_html_attrs]["value"] = value
+
+        correct_value: Any | None = value
+        if html_attrs["group"] == "date":
+            correct_value = parse(value)
+
+        instance.__dict__[self.internal_name] = correct_value
+        instance.__dict__[field_name_html_attrs]["value"] = correct_value
