5afb5e1ce9dc85fef788f88d5dd24dde854c913d
develgen <develgen.dev@gmail.com>
2026-07-25 09:06:03 +0300

    update

--- tests/final/test_basic_2.py
+++ tests/final/test_basic_2.py
@@ -190,7 +190,7 @@ class TestBasicExample(unittest.IsolatedAsyncioTestCase):
 
         self.assertEqual(user.lang_code, "ru")
 
-        user_details = await User.find_one_to_instance_model({"_id": user.id}, user.lang_code)
+        user_details = await User.find_one_to_instance_model({"_id": user.id}, "ru")
         self.assertIsNotNone(user_details)
         self.assertEqual(user_details.created_at, user.created_at)
         self.assertEqual(user_details.updated_at, user.updated_at)
