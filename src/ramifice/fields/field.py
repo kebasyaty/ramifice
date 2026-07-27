5340ad94c55e435ec8fef92241e1747eccf372bf
develgen <develgen.dev@gmail.com>
2026-07-27 06:42:13 +0300

    fix FieldCore

--- src/ramifice/fields/field.py
+++ src/ramifice/fields/field.py
@@ -30,7 +30,6 @@ from typing import Any
 from dateparser import parse
 
 from ramifice.errors import AttributeCannotBeDeleteError
-from ramifice.translator import Translator
 
 logger = logging.getLogger(__name__)
 
@@ -39,8 +38,6 @@ class FieldCore:
     """A class for carrying field arguments and methods."""
 
     def __init__(self, **kwargs: dict[str, Any]) -> None:
-        self.current_locale = Translator.DEFAULT_LOCALE
-
         for key, value in kwargs.items():
             self.__dict__[key] = value
 
